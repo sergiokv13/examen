40f9535b2b28a4c77e9d9f696b4b820b8ac24b2f
sergiokv13 <sergiokv13@gmail.com>
2013-12-03 08:02:38 -0400

    Respuesta 5-App Web

--- src/main/java/examen/principalServlet.java
+++ src/main/java/examen/principalServlet.java
@@ -9,12 +9,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.Scanner;
 import java.io.PrintWriter;
 public class principalServlet extends HttpServlet {
+	anho a = new anho();
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 				response.setContentType("text/html");
 				PrintWriter out = response.getWriter();
 				int anho =Integer.parseInt ( request.getParameter("anho") );
-				out.println(anho);
+				if ( a.anhoBisiesto(anho) )
+					out.println("El anho es Bisiesto");
+				else
+					out.println("El anho no es Bisiesto");
+
+				out.println("<a href=\"/\" > Atras </a>");
 	}
 }
